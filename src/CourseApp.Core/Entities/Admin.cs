7074876db68cb7933c2b1c94e1f18aebf01c9ad3
User <User@NAHIDUL>
2026-04-05 01:48:51 -0700

    Add project files.

---
+++ src/CourseApp.Core/Entities/Admin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseApp.Core.Entities;
+
+public class Admin
+{
+    public int Id { get; set; }
+
+    public string Code { get; set; } = default!;
+
+    public string PasswordHash { get; set; } = default!;
+
+    public string DriveNumber { get; set; } = default!;
+
+    public bool MustResetPassword { get; set; } = true;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ src/CourseApp.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CourseApp.Desktop.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
